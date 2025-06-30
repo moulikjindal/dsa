9a45e5280d55510570a9d3ab6bd6094497f16da4
moulikjindal <100513188+moulikjindal@users.noreply.github.com>
2025-07-01 00:30:08 +0530

    done

--- LC_643.cpp
+++ LC_643.cpp
@@ -1,1 +1,24 @@
 
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        vector<double> dup; 
+        for (auto num : nums){
+            dup.push_back(double(double(num)/k));
+        }
+        double maxSum = 0;
+        double currentSum = 0;
+        //window size k
+        for (int i = 0; i < k; i++) {
+            currentSum += dup[i];
+        }
+        //
+        maxSum = currentSum;
+        //moving the window
+        for (int i = k; i < dup.size(); i++) {
+            currentSum += dup[i] - dup[i - k];
+            maxSum = max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+};
