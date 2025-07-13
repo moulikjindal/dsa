260382dbb4d51d35581f2fe3a63512b1419b97e3
moulikjindal <100513188+moulikjindal@users.noreply.github.com>
2025-07-14 01:16:53 +0530

    k

--- Graph/LC_1971.cpp
+++ Graph/LC_1971.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
-    vector<int> visited(n, 0);
+    vector<vector<int>> adjacencyList;
+    vector<bool> visited;
+    bool traverse(int source, int destination, vector<vector<int>>& adjacencyList, int n) {
+        // base case
+        if (visited[source]) {
+            return false;
+        }
+        if (source == destination) {
+            return true;
+        }
+        visited[source] = true;
+        for (int i=0; i < adjacencyList[source].size(); i++) {
+            if (!visited[adjacencyList[source][i]]) {
+                if (traverse(adjacencyList[source][i], destination, adjacencyList, n)) {
+                    return true;
+                }
+            }
+        }
+        return false;
 
+    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         //graph is bidirectional
-        if (source == destination) return true;
-        visited[source] = 1;
-        //the validPath function checks if there is a valid path from source to destination in an undirected graph.
-        for (int i = 0; i < n; i++) {
-            validPath(n, edges, edges[source][i], destination);
-        }   
+        visited.resize(n, false);
+        adjacencyList.resize(n);
+        for (const auto& edge : edges) {
+            adjacencyList[edge[0]].push_back(edge[1]);
+            adjacencyList[edge[1]].push_back(edge[0]);
+        }
+
+        return traverse(source, destination, adjacencyList, n);
     }
 };
